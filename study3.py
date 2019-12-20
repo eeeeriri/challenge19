01815bc0b7c87648e410322f8a22f2a5568ccf80
kubo erika <Erika1_Kubo@toshibatec.co.jp>
2019-12-20 18:45:53 +0900

    beginner problem

---
+++ study3.py
@@ -0,0 +1,41 @@
+#1
+print("Hello!")
+print("kubo")
+print("kubokubo")
+
+#2
+i = 10
+if i < 10:
+    print("under10")
+else:
+    print("over10")
+
+#3
+num = 2
+if num < 10:
+    print("under10")
+elif 10 < num <= 25:
+    print("under10_under25")
+else:
+    print("over25")
+
+#4
+a = 17
+b = 6
+ans4 = a % b
+print (ans4)
+#5
+c = 22
+d = 4
+ans5 = c / d
+print (ans5)
+# #6
+age = 20
+if age < 7:
+    print("infant")
+elif 7 <= age < 20:
+    print("children")
+else:
+    print("adoult")
+
+    
